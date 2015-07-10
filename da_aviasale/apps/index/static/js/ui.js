19d5ac165a6876dbd94c865036c27841fa2c4edc
Andrew Pogrebnjak <fashust.nefor@gmail.com>
2015-07-10 12:22:23 +0300

    ui validation errors and some fixes in OrderForm validation

--- da_aviasale/apps/index/static/js/ui.js
+++ da_aviasale/apps/index/static/js/ui.js
@@ -10,18 +10,46 @@ String.prototype.format = function() {
     return formatted;
 };
 
-var form_selector = '#search-form',
+var search_form_selector = '#search-form',
     search_results_holder = '.results',
     buy_btn = '.buy',
     popup_selector = '#orderPopup',
     result_selector = 'div[data-id="{0}"]',
     order_btn = '#order',
     order_form = 'form[name=order]',
-    result_item_tpl = '<div data-id={0}><span>{1}</span><span>{2}</span>' +
+    result_item_tpl = '<div class="results_row" data-id={0}><span>{1}</span><span>{2}</span>' +
         '<span>{3}</span><span>{4}</span>' +
-        '<a class="buy" href="#" data-places={5} data-cost={6}>buy</a></div>';
+        '<a class="buy" href="#" data-places={5} data-cost={6}>buy</a></div>',
+    error_msg_tpl = '<span class="{0}_error" style="color: red;">{1}</span>';
+
+function show_form_errors(selector, messages, clear) {
+    var form = $(selector);
+
+    if (clear) {
+        _.each(form.find('input, select'), function(item) {
+            var field_name = $(item).attr('name');
+            if (field_name != undefined) {
+                $('.' + field_name + '_error').remove();
+            }
+        });
+    } else {
+        _.each(_.keys(messages), function(key) {
+            var field_messages = _.map(messages[key], function(message) {
+                return error_msg_tpl.format(key, message.message);
+            }).join(', ');
+            console.log(field_messages);
+            console.log();
+            if (form.find('.' + key + '_error').length > 0) {
+                form.find('.' + key + '_error').replaceWith(field_messages);
+            } else {
+                form.find('[name=' + key + ']').after(field_messages);
+            }
+        });
+    }
+}
 
 function search_results(response) {
+    show_form_errors(search_form_selector, null, true);
     $(search_results_holder).html();
     if (response.status) {
         var data = response.data.results,
@@ -43,17 +71,16 @@ function search_results(response) {
             $(search_results_holder).html('<h3>no results</h3>');
         }
     } else {
-        console.log('show form errors')
+        show_form_errors(search_form_selector, response.errors, false);
     }
 }
 
 function show_order_popup(flight_id) {
-    console.log('show popup');
-    console.log(flight_id);
     var result = $(result_selector.format(flight_id)).find('a').data();
+
     $(popup_selector).find('input[name=flight]').val(flight_id);
     $(popup_selector).find('input[name=places]').val(result.places);
-    $(popup_selector).find('input[name=cost]').val(result.cost / result.places);
+    $(popup_selector).find('input[name=cost]').val((result.cost / result.places).toFixed(2));
     $(popup_selector).find('input[name=total_cost]').val(result.cost);
     $(popup_selector).modal();
 }
@@ -64,19 +91,35 @@ function success_lock(response) {
     }
 }
 
+function success_order(response) {
+    show_form_errors(order_form, null, true);
+    if (response.status) {
+        alert('THNX');
+    } else {
+        show_form_errors(order_form, response.errors, false);
+    }
+}
+
+function clear_popup() {
+    var order_popup = $(popup_selector);
+    _.each(order_popup.find('input').not('[name=csrfmiddlewaretoken]'), function(item) {
+        $(this).val('');
+    });
+}
+
 $(document).ready(function() {
     $('#id_date').datepicker();
 
-    $(document).on('click', form_selector + ' > input[type=submit]', function(evt) {
+    $(document).on('click', search_form_selector + ' > input[type=submit]', function(evt) {
         evt.preventDefault();
 
         $.ajax({
             type: 'POST',
-            url: $(form_selector).attr('action'),
-            data: $(form_selector).serialize(),
+            url: $(search_form_selector).attr('action'),
+            data: $(search_form_selector).serialize(),
             success: search_results,
-            failure: function(errMsg) {
-                console.log(errMsg);
+            failure: function(jqXHR, textStatus, errorThrown) {
+                console.log(textStatus);
             }
         });
     });
@@ -87,8 +130,8 @@ $(document).ready(function() {
             type: 'GET',
             url: '/search/lock/?lock={0}'.format($(this).parent().data('id')),
             success: success_lock,
-            failure: function(errMsg) {
-                console.log(errMsg);
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(textStatus);
             }
         })
     });
@@ -104,11 +147,9 @@ $(document).ready(function() {
             type: 'POST',
             url: $(order_form).attr('action'),
             data: $(order_form).serialize(),
-            success: function(response) {
-                console.log(response);
-            },
-            failure: function(errMsg) {
-                console.log(errMsg);
+            success: success_order,
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(textStatus);
             }
         });
     });
--- da_aviasale/apps/orders/forms.py
+++ da_aviasale/apps/orders/forms.py
@@ -49,17 +49,25 @@ class FlightOrderForm(forms.Form):
             clean form data
         """
         cleaned_data = super(FlightOrderForm, self).clean()
-        flight = cleaned_data['flight']
-        places = cleaned_data['places']
-        cost = cleaned_data['cost']
-        total_cost = cleaned_data['total_cost']
-        if not flight.available_seats or flight.available_seats < places:
+        flight = cleaned_data.get('flight', None)
+        places = cleaned_data.get('places', None)
+        cost = cleaned_data.get('cost', None)
+        total_cost = cleaned_data.get('total_cost', None)
+        if (
+                flight and
+                places and
+                (
+                    not flight.available_seats or
+                    flight.available_seats < places
+                )
+        ):
             self.add_error('flight', 'no available places')
-        if not flight.is_locked:
+        if flight and not flight.is_locked:
             self.add_error('flight', 'flight is not locked')
-        if flight.cost != cost:
+        if flight and cost and flight.cost != cost:
             self.add_error('cost', 'cost did not match flight cost')
         if (
+                flight and places and total_cost and
                 (flight.cost * places).quantize(Decimal('0.00')) !=
                 total_cost.quantize(Decimal('0.00'))
         ):
